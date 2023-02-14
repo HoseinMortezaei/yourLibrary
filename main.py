45f305751c6a8367e0505f4ceb3fcaf007f72bb4
Hosein Mortezaei <hosein.mortezaei1385@gmail.com>
2023-02-14 21:54:17 +0330

    good night!

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys
 from PySide2.QtWidgets import QApplication, QMainWindow
 from PySide2.QtCore import QFile
+from PySide2.QtUiTools import QUiLoader
 
 class ThingAdd():
     def __init__(self):
@@ -8,18 +9,30 @@ class ThingAdd():
 
 class BookAdd():
     def __init__(self):
-        pass 
+        ui_file = QFile("bookAdd.ui")
+        ui_file.open(QFile.ReadOnly)
+        loader = QUiLoader()
+        window = loader.load(ui_file)
+        window.show()
+        sys.exit(app.exec_())
 
 class MainWindow():
     def __init__(self):
         ui_file = QFile("main.ui")
         ui_file.open(QFile.ReadOnly)
         loader = QUiLoader()
-        window = loader.load(ui_file)
-        window.show()
+        self.mainWindow = loader.load(ui_file)
+        ####
+        self.HomeBtn()
+        ####
+        self.mainWindow.show()
+        sys.exit(app.exec_())
+
+    def HomeBtn(self):
+        self.mainWindow.addBook.clicked.connect(lambda:BookAdd())
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
-    sys.exit(app.exec_())
+    
