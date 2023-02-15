d75dc86e1321c80bbc44251c5dcf6b3a4b44c9d2
Hosein Mortezaei <hosein.mortezaei1385@gmail.com>
2023-02-15 22:23:52 +0330

    feb 15 submit after adding a little book add

---
+++ fileProc.py
@@ -0,0 +1,11 @@
+def read(number):
+    file = open(f'{number}.blf')
+    lines = file.read().splitlines()
+    book = eval(lines[2:3])
+    return book
+def write(name,writer,number,year,star,additional,category,group,publisher):
+    file = open(f'LibraryAppBooks\\{number}.blf','w')
+    x = {'name':name,'writer':writer,'number':number,'year':year,'star':star,'additional':additional,'category':category,'group':group,'publisher':publisher}
+    file.write(f'v1.0\n--begin--\n{str(x)}\n--end--')
+    file.close()
+    return True
--- main.py
+++ main.py
@@ -2,19 +2,8 @@ import sys
 from PySide2.QtWidgets import QApplication, QMainWindow
 from PySide2.QtCore import QFile
 from PySide2.QtUiTools import QUiLoader
-
-class ThingAdd():
-    def __init__(self):
-        pass
-
-class BookAdd():
-    def __init__(self):
-        ui_file = QFile("bookAdd.ui")
-        ui_file.open(QFile.ReadOnly)
-        loader = QUiLoader()
-        window = loader.load(ui_file)
-        window.show()
-        sys.exit(app.exec_())
+from fileProc import read,write
+import os
 
 class MainWindow():
     def __init__(self):
@@ -23,13 +12,38 @@ class MainWindow():
         loader = QUiLoader()
         self.mainWindow = loader.load(ui_file)
         ####
+        ui_file = QFile("bookAdd.ui")
+        ui_file.open(QFile.ReadOnly)
+        loader = QUiLoader()
+        self.bookAdd = loader.load(ui_file)
+        ####
         self.HomeBtn()
+        self.bookAddWin()
         ####
         self.mainWindow.show()
         sys.exit(app.exec_())
 
     def HomeBtn(self):
-        self.mainWindow.addBook.clicked.connect(lambda:BookAdd())
+        self.mainWindow.addBook.clicked.connect(self.bookAdd.show)
+
+    def bookAddWin(self):
+        if not('LibraryAppBooks' in os.listdir()):
+            os.mkdir('LibraryAppBooks')
+        def okClicked():
+            x = self.bookAdd.number.text()
+            if f'{x}.blf' in os.listdir():
+                #book with this number is already exist message
+                pass
+            else:
+                if not (write(self.bookAdd.name.text(),self.bookAdd.writer.text(),self.bookAdd.number.text(),self.bookAdd.year.text(),self.bookAdd.staryes.isChecked(),self.bookAdd.additional.toPlainText(),self.bookAdd.cate.currentText(),self.bookAdd.group.currentText(),self.bookAdd.publisher.currentText())):
+                    #something went wrong message
+                    pass                    
+
+                # successfully added book message
+                self.bookAdd.close()
+        self.bookAdd.ok.clicked.connect(okClicked)
+        self.bookAdd.discard.clicked.connect(self.bookAdd.close)
+
 
 
 if __name__ == "__main__":
